0cab80ccbec19d1eea497a05bd61036b3421743f
justyn temme <justyntemme@gmail.com>
2024-09-03 18:50:15 -0500

    build testing framework

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set environment variables
+export CLOUD_ACCOUNT="ZOOM-SANDBOX-2"
+export CIDR_IPS="192.168.0.1,192.168.0.2"
+export VPC_ID="vpc-2345"
+
+# Run the Python script
+python main.py
