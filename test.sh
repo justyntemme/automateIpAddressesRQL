b2c2028fb04c85a242fc128722d003a69c156592
justyn temme <justyntemme@gmail.com>
2024-09-04 10:48:38 -0500

    working but still have cleanup TODO

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Set environment variables
 export CLOUD_ACCOUNT="ZOOM-SANDBOX-2"
-export CIDR_IPS="192.168.0.1,192.168.0.2"
 export VPC_ID="vpc-2345"
+export SECURITY_GROUPS="sg-123456,sg-54321"
+export CIDR_IPS="192.168.0.1,192.168.0.2"
 
-# Run the Python script
 python main.py
